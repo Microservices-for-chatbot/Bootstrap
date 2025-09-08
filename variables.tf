ea98ff02a2b68d1b12dbe39f29cb6c00e47481aa
Amith P <107483173+amithpalissery@users.noreply.github.com>
2025-09-08 17:37:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "The EC2 instance type."
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "public_key" {
