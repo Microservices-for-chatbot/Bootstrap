dff0bd9b8e3752785de18c6a917c68cd6f01fafd
Amith P <107483173+amithpalissery@users.noreply.github.com>
2025-09-10 15:21:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,8 @@ resource "aws_instance" "runner_instance" {
     ]
   }
 }
+
+output "instance_public_ip" {
+  value = aws_instance.runner_instance.public_ip
+  description = "The public IP address of the EC2 instance."
+}
