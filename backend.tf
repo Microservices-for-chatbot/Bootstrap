60f7ec5396b29c07dd708ca87646162e8b2b1b12
Amith P <107483173+amithpalissery@users.noreply.github.com>
2025-09-11 16:32:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,38 +1,9 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "amith_ms"  
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    id      = "cleanup-old-versions"
-    enabled = true
-
-    noncurrent_version_expiration {
-      days = 30
-    }
-  }
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Production"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform Locks Table"
-    Environment = "Production"
+terraform {
+  backend "s3" {
+    bucket         = var.s3_bucket_name
+    key            = "state/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = var.dynamodb_table_name
+    encrypt        = true
   }
 }
